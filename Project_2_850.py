671ab7d62d41dd553ab1c78b2a8ee64f254650cb
Josh. M <josh.macdonald@torontomu.ca>
2024-11-06 16:12:51 -0500

    Added base model

--- Project_2_850.py
+++ Project_2_850.py
@@ -3,6 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pathlib
 
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+from tensorflow.keras.models import Sequential
+
 import keras
 
 
@@ -42,3 +46,35 @@ valid_ds = keras.preprocessing.image_dataset_from_directory(
     label_mode='categorical',
     batch_size=32,
     image_size=(500, 500))
+
+
+
+model = Sequential()
+
+#Rescale Layer
+model.add(layers.Rescaling(1./255, input_shape=(500, 500, 3)))
+
+#Conv2D & Pooling Layer
+model.add(layers.Conv2D(16, (3, 3), activation='relu'))
+model.add(layers.MaxPooling2D(pool_size=(2, 2)))
+
+#Flatten Layer
+model.add(layers.Flatten())
+
+#Dense Layer 1
+model.add(layers.Dense(32, activation='relu'))
+
+
+#Final Dense Layer
+model.add(layers.Dense(3, activation='softmax'))
+
+
+
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+
+
+history = model.fit(train_ds, epochs=1, batch_size=32,validation_data=valid_ds)
+
+test_loss, test_acc = model.evaluate(test_ds)
+print(f'Test accuracy: {test_acc:.4f}')
